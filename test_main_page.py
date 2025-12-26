db2ecbcd14e3f84c8cc14f3cfac95b21ef227d06
Astra Blur <astrablur@gmail.com>
2025-12-26 17:00:35 +0300

    Test class for main page tests and pytest.ini added

--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pages.basket_page import BasketPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
@@ -5,19 +7,20 @@ from pages.main_page import MainPage
 link = 'https://selenium1py.pythonanywhere.com/'
 
 
-def test_guest_should_see_login_link(browser):
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_should_see_login_link(self, browser):
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.should_be_login_link()
 
+    def test_guest_can_go_to_login_page(self, browser):
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_login_page()
 
-def test_guest_can_go_to_login_page(browser):
-    main_page = MainPage(browser, link)
-    main_page.open()
-    main_page.go_to_login_page()
-
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
